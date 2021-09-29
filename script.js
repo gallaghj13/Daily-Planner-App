801dbe8128b9aec5ec3bec64220cd00a4b4d1ead
John Gallagher <gallagherj0913@gmail.com>
2021-09-29 00:23:56 -0600

    updated js, simplified some of the functions

--- script.js
+++ script.js
@@ -2,95 +2,108 @@ var hour = document.querySelectorAll(".hour");
 var conatinerEl = document.querySelector(".conatiner");
 var saveBtnEl = document.querySelectorAll(".saveBtn");
 var textareaEls = document.querySelectorAll("textarea");
-var spanEl = document.querySelectorAll("span");
+var spanEl = document.querySelectorAll("t");
 
-var scheduledEventText = JSON.parse(localStorage.getItem("scheduled-event"));
+
 var scheduledEventText = textareaEls;
 
-var inputArray = [];
 
 
 // Need time to display underneath the main banner header
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+localStorage.setItem("today", today);
 
 // An event listener for the save buttons
 
-$("#container-element").on('click', '.saveBtn', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-    var textInput = $('span').prev("textarea").val();
-    var textID = $('span').prev("textarea").attr("id");
-    // console.log(textID);
+$(".saveBtn").on('click', function(event) {
+    // event.siblingsentDefault();
+    // event.stopPropagation();
+    // event.stopImmediatePropagation();
+    var description = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    console.log(time, description);
+    localStorage.setItem(time, description)
     // var textObj.textID = textID;
     // textObj[textID] = textID;
     // localStorage.setItem("yourKey", JSON.stringify(textOBJ));
-    var textObj = {
-        id: textID,
-        input: textInput,
-    };
-    inputArray.push(textObj);
-    localStorage.setItem("ID-Input-Pair", JSON.stringify(textObj));
+    // var textObj = {
+    //     id: textID,
+    //     input: textInput,
+    // };
+    // inputArray.push(textObj);
+    // localStorage.setItem("ID-Input-Pair", JSON.stringify(textObj));
 
-    localStorage.setItem("input-Array", JSON.stringify(inputArray));
+    // localStorage.setItem("input-Array", JSON.stringify(inputArray));
     
-    localStorage.setItem("scheduled-eventID", JSON.stringify(textID));
+    // localStorage.setItem("scheduled-eventID", JSON.stringify(textID));
 
-    localStorage.setItem("scheduled-event", JSON.stringify(textInput));
-    $(textID).textContent = textInput;
-    scheduledEvent();
+    // localStorage.setItem("scheduled-event", JSON.stringify(textInput));
+    // $(textID).textContent = textInput;
+    // scheduledEvent();
     // need to call a save text function
 });
-// A function to save the input activity and keep it there, use of local storage
-// Also need to make sure it is there upon refreshing the page
-function scheduledEvent() {
-    // localStorage.setItem("scheduled-event", scheduledEvent);
-
-    var scheduledEventText = JSON.parse(localStorage.getItem("input-Array"));
-    return scheduledEventText;
-};
 
 // A function that tracks the time and changes the colors of the input field, grey in the past, red current, green future
+
 function textAreaColor() {
-    var now = moment();
-    var time = now.hour();
-    console.log(time);
-    for (var i=0; i < spanEl.length; i++) {
-        var  timeOnPlanner = spanEl[i].textContent;
-        console.log(timeOnPlanner);
-        console.log(time);
+    var currentHour = moment().hours();
+    console.log(currentHour);
+    // for (var i=0; i < spanEl.length; i++) {
+    //     var  timeOnPlanner = spanEl[i].textContent;
+    //     console.log(timeOnPlanner);
+    //     console.log(time);
 
-    $('span').each(function() {
-
-        if (timeOnPlanner > time) {
-            $('span').eq(i).next().removeClass("present past").addClass("future");
+    $('.description').each(function() {
+        var timeOnPlanner = parseInt($(this).parent().attr("id").split('-')[1]);
+        console.log(timeOnPlanner)
+        if (timeOnPlanner < currentHour) {
+            $(this).removeClass("present")
+            $(this).removeClass('future')
+            $(this).addClass("past");
         }
-        else if (timeOnPlanner == time) {
-            $('span').eq(i).next().removeClass("future past").addClass("present");
+        else if (timeOnPlanner === currentHour) {
+            $(this).removeClass("past")
+            $(this).removeClass("future")
+            $(this).addClass("present");
         }
         else {
-            $('span').eq(i).next().removeClass("present future").addClass("past");
+            $(this).removeClass("present")
+            $(this).removeClass("past")
+            $(this).addClass("future");
         }
         
-})}};
+})};
 
 
 // Need a function that resets all fields at a certain time, might as well be 6 since the planner ends at 5
 // Might need to set an interval to reset content
-function newDay() { 
-    var now = moment();
-    var time = now.hour();
-    if (time < 1) {
-        document.querySelector(".container").reset(); 
-    }
-};
+
+// Thought is to use moment().isAfter(siblingsiousday) then reset everything
+// function newDay() { 
+//     var now = moment();
+//     var currentTime = now.hour;
+//     var initialTime = localStorage.getItem("today").val;
+//     if (currentTime.day > initialTime.day) {
+//         document.querySelector(".container").reset(); 
+//     }
+// };
+
+$('#hour-9 .description').val(localStorage.getItem("hour-9"))
+$('#hour-10 .description').val(localStorage.getItem("hour-10"))
+$('#hour-11 .description').val(localStorage.getItem("hour-11"))
+$('#hour-12 .description').val(localStorage.getItem("hour-12"))
+$('#hour-13 .description').val(localStorage.getItem("hour-13"))
+$('#hour-14 .description').val(localStorage.getItem("hour-14"))
+$('#hour-15 .description').val(localStorage.getItem("hour-15"))
+$('#hour-16 .description').val(localStorage.getItem("hour-16"))
+$('#hour-17 .description').val(localStorage.getItem("hour-17"))
 
 
 // Initialize the site by bringing up the saved text and checking time
+
 function init() {
     textAreaColor();
-
 };
 
 init();
